8ebe77a3f69b7a5a17c783f2af8f17d833534030
janghohan <wkdgh5430@naver.com>
2024-06-30 09:49:46 +0900

    첫번째

---
+++ login.php
@@ -0,0 +1,59 @@
+
+<html lang="ko" class=" ">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="canonical" href="https://www.sellertool.io">
+      <meta name="description" content="마진율 계산기 부터 재고관리 까지 오픈마켓 통합 관리 솔루션 셀러툴!">
+      <meta name="keywords" content="셀러툴, 마진율 계산기, 엑셀 변환기, 스마트스토어, 내 상품 순위, 온라인 커머스, 3PL, WMS, 선입선출, 주문관리">
+      <meta property="og:type" content="website">
+      <meta property="og:title" content="셀러툴 - 쇼핑몰 통합관리">
+      <meta property="og:description" content="마진율 계산기 부터 재고관리 까지 오픈마켓 관리 솔루션 셀러툴!">
+      <meta property="og:site_name" content="셀러툴 - 쇼핑몰 통합관리">
+      <meta property="og:url" content="https://www.sellertool.io">
+      <meta property="og:locale" content="ko_KR">
+      <meta name="naver-site-verification" content="5abe3c64078f2d1754ecfd2de8327d7e31542d4c">
+      <title>로그인 | 셀러툴 - 쇼핑몰 통합관리</title>
+      <link rel="stylesheet" type="text/css" href="./css/login.css" data-n-g="">
+
+   </head>
+   <body style="">
+      <div id="__next" data-reactroot="">
+         <div class="login_Container">
+            <div class="FormField__Container">
+               <div class="FormField_logobox">
+                  <a href="/">
+                     <span style="box-sizing: border-box; display: block; overflow: hidden; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative;">
+                        <span style="box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 100% 0px 0px;">
+                        </span>
+                        <img alt="image" src="">
+                     </span>
+                  </a>
+               </div>
+               <div class="FormField__Wrapper">
+                  <div style="margin-top: 40px; text-align: center; font-weight: 600; font-size: 18px;">로그인</div>
+                  <form class="FormField__FormGroup">
+                     <div class="FormField__InputBox">
+                        <input type="text" class="input-item" name="username" placeholder="아이디" required="" value="">
+                     </div>
+                     <div class="FormField__InputBox" style="margin-top: 20px;">
+                        <input type="password" class="input-item" name="password" placeholder="패스워드" required="" value="">
+                     </div>
+                     <div class="find-link-group">
+                        <span class="username" href="/find/username/">아이디</span>
+                        <span class="password" href="/find/password/">비밀번호 찾기</span>
+                     </div>
+                     <button type="submit" class="SingleBlockButton__Button-sc-18olea5-0 ipcWoQ submit-button">
+                        로그인
+                        <div color="#e0e0e0" scale="2" class="Ripple__RippleContainer-sc-1gpqkoy-0 hafhZp"></div>
+                     </button>
+                  </form>
+                  <div style="text-align: center; font-size: 13px; color: rgb(128, 128, 128); margin-top: 40px; margin-bottom: 80px;">회원이 아니신가요? 
+                     <span href="/signup/" style="text-decoration: underline; cursor: pointer;">회원가입</span>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      </body>
+</html>
